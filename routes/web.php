d3b215213544d0b5be3122b4ad9fc2846a6fd194
mohamad <tajik2421@gmail.com>
2021-09-04 14:11:21 +0430

    web templates added to the project.

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('client.home');
+});
+
+route::get('/adminpanel' , function(){
+   return view('admin.home') ;
 });
